16a976bae8f974d19bc2e77ec23bf64012d9829d
Nick Skriloff <skriloffn@darden.virginia.edu>
2022-08-03 08:59:10 -0400

    add path to mp4 in script

--- launchManim.ps1
+++ launchManim.ps1
@@ -1,5 +1,7 @@
 # TODO my verision of Manim on github
 #docker run --rm -v /Users/skriloffn/devhelp/manim/manim/example_scenes/:/manim manimcommunity/manim manim basic.py SquareToCircle -qm
-docker run --rm -v $PSScriptRoot/manim/example_scenes/:/manim manimcommunity/manim manim basic.py SquareToCircle -qm
+docker run --rm -v $PSScriptRoot/example_scenes/:/manim manimcommunity/manim manim basic.py SquareToCircle -qm
 #pipe the output of this to an object so you have the path available. 
-open $PSScriptRoot/manim/example_scenes/media/videos/basic/720p30/SquareToCircle.mp4
+$pathToMp4 =(Get-ChildItem -Path . -Recurse -Name SquareToCircle.mp4)
+Start-Process -FilePath $pathToMp4
+
