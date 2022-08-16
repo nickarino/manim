02ddc27ac727e31adfbc9e061d736e186b26bdc4
Nick Skriloff <skriloffn@darden.virginia.edu>
2022-08-16 08:38:59 -0400

    fix string interpolation to have  instead of

--- launchManim.ps1
+++ launchManim.ps1
@@ -1,6 +1,6 @@
 # TODO my verision of Manim on github
 #docker run --rm -v /Users/skriloffn/devhelp/manim/manim/example_scenes/:/manim manimcommunity/manim manim basic.py SquareToCircle -qm
-docker run --rm -v $PSScriptRoot/example_scenes/:/manim manimcommunity/manim manim basic.py SquareToCircle -qm
+docker run --rm -v ${PSScriptRoot}/example_scenes/:/manim manimcommunity/manim manim basic.py SquareToCircle -qm
 #pipe the output of this to an object so you have the path available. 
 $fileName = "SquareToCircle.mp4"
 $pathToMp4 = Split-Path (Get-ChildItem -Path . -Recurse -Name $fileName)
