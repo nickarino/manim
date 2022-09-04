ffaf734caabc8a2dfbffaa1aec33d786bd931d51
Nick Skriloff <skriloffn@darden.virginia.edu>
2022-09-03 23:20:36 -0400

    quickstart.py

--- nick/quickStart.py
+++ nick/quickStart.py
@@ -32,3 +32,27 @@ class SquareAndCircle(Scene):
 
         square.next_to(circle, UP, buff=0.5)  # set the position
         self.play(Create(circle), Create(square))  # show the shapes on screen
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+        
